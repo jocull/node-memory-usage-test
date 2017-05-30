64bd6e1c9537f117f368cf7db7b64514a8187406
James O'Cull <jocull@delmarsd.com>
2017-05-30 18:41:21 -0400

    Async version with worker farm

--- index.js
+++ index.js
@@ -1,67 +1,77 @@
 const _ = require('lodash');
 const faker = require('faker');
+const Promise = require('bluebird');
+const workerFarm = require('worker-farm');
+const wordGenWorker = workerFarm(require.resolve('./word-gen'));
 
-const makeText = (() => {
-  const paras = 20;
-  const textFunctions = [
-    () => faker.lorem.paragraphs(paras),
-    () => _.times(paras * 10).map(faker.company.bs).join(' '),
-    () => _.times(paras * 4).map(faker.hacker.phrase).join(' '),
-    () => _.times(paras).map(i => [
-        faker.name.firstName() + ' ' + faker.name.lastName(),
-        faker.address.streetAddress(),
-        faker.address.city() + ', ' + faker.address.stateAbbr() + ' ' + faker.address.zipCode(),
-      ].join('\n')).join('\n\n'),
-  ];
-
-  return () => _.sample(textFunctions)();
-})();
-
-const ITERATIONS = 100000;
+const ITERATIONS = 500000;
 let lastMs = Date.now();
-const result =
-  _.chain(_.range(0, ITERATIONS))
-    .tap(() => console.log('Mapping!'))
-    .reduce((acc, i) => {
-      if (i % 10000 === 0) {
-        let now = Date.now();
-        let mem = process.memoryUsage();
-        console.log();
-        console.log('Iteration', i.toLocaleString());
-        console.log('Resident set', (mem.rss / 1024 / 1024).toLocaleString(), 'MB');
-        console.log('Heap usage', (mem.heapUsed / 1024 / 1024).toLocaleString(), 'MB');
-        console.log('Heap total', (mem.heapTotal / 1024 / 1024).toLocaleString(), 'MB');
-        console.log('Time to chunk', (now - lastMs).toLocaleString(), 'ms');
-        lastMs = now;
-      }
 
-      const text = makeText();
-      const words = text.match(/\w+/g);
-      const val = {
-        text,
-        words: () => words,
-        lowerWords: () => words.map(x => x.toLowerCase()),
-        count: () => words.length,
-      };
-
-      for (let word of val.words()) {
-        if (word in acc) {
-          acc[word]++;
-        } else {
-          acc[word] = 1;
+let wordGenPromises = _.range(0, ITERATIONS)
+  .map(i => {
+    return new Promise((resolve, reject) => {
+      wordGenWorker(null, (err, ok) => {
+        if (i % 10000 === 0) {
+          let now = Date.now();
+          let mem = process.memoryUsage();
+          console.log();
+          console.log('Iteration', i.toLocaleString());
+          console.log('Resident set', (mem.rss / 1024 / 1024).toLocaleString(), 'MB');
+          console.log('Heap usage', (mem.heapUsed / 1024 / 1024).toLocaleString(), 'MB');
+          console.log('Heap total', (mem.heapTotal / 1024 / 1024).toLocaleString(), 'MB');
+          console.log('Time to chunk', (now - lastMs).toLocaleString(), 'ms');
+          lastMs = now;
         }
-      }
-      return acc;
-    }, {})
-    .tap(() => console.log('Sorting!'))
-    .map((val, key) => ({
-      word: key,
-      count: val,
-    }))
-    .sortBy(x => x.count)
-    .tap(() => console.log('Reversing!'))
-    .reverse()
-    .take(20)
-    .value();
 
-console.log(result);
+        if (err) {
+          reject(err)
+        } else {
+          resolve(ok);
+        }
+      });
+    });
+  });
+
+Promise.reduce(wordGenPromises, (acc, text) => {
+  const words = text.match(/\w+/g);
+  const val = {
+    text,
+    words: () => words,
+    lowerWords: () => words.map(x => x.toLowerCase()),
+    count: () => words.length,
+  };
+
+  for (let word of val.words()) {
+    if (word in acc) {
+      acc[word]++;
+    } else {
+      acc[word] = 1;
+    }
+  }
+  return acc;
+}, {})
+  .then(wordAcc => {
+    return _.chain(wordAcc)
+      .map((val, key) => ({
+        word: key,
+        count: val,
+      }))
+      .sortBy(x => x.count)
+      .reverse()
+      .take(40)
+      .value();
+  })
+  .then(result => {
+    let mem = process.memoryUsage();
+    console.log();
+    console.log('======End======');
+    console.log('Resident set', (mem.rss / 1024 / 1024).toLocaleString(), 'MB');
+    console.log('Heap usage', (mem.heapUsed / 1024 / 1024).toLocaleString(), 'MB');
+    console.log('Heap total', (mem.heapTotal / 1024 / 1024).toLocaleString(), 'MB');
+
+    console.log(result);
+  })
+  .catch(err => console.error(err)) // Move to end and teardown
+  .then(() => {
+    workerFarm.end(wordGenWorker);
+  });
---
+++ word-gen.js
@@ -0,0 +1,18 @@
+const _ = require('lodash');
+const faker = require('faker');
+
+const paras = 4;
+const textFunctions = [
+    () => faker.lorem.paragraphs(paras),
+    () => _.times(paras * 10).map(faker.company.bs).join(' '),
+    () => _.times(paras * 4).map(faker.hacker.phrase).join(' '),
+    () => _.times(paras).map(i => [
+        faker.name.firstName() + ' ' + faker.name.lastName(),
+        faker.address.streetAddress(),
+        faker.address.city() + ', ' + faker.address.stateAbbr() + ' ' + faker.address.zipCode(),
+    ].join('\n')).join('\n\n'),
+];
+
+module.exports = function (input, callback) {
+    callback(null, _.sample(textFunctions)());
+}
